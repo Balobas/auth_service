5fe4542636911424899637aa2c20bf76fc52f6e9
vbalobanov <vbalobanov@innoseti.ru>
2024-08-13 15:17:11 +0300

    implement grpc server with stubs

---
+++ cmd/server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	deliveryGrpc "balun/auth_service_bln/internal/delivery/grpc"
+	"balun/auth_service_bln/pkg/auth_v1"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	grpcPort = "50051"
+)
+
+func main() {
+	conn, err := net.Listen("tcp", grpcPort)
+	if err != nil {
+		log.Fatalf("failed to listen: %v\n", err)
+	}
+
+	server := grpc.NewServer()
+	reflection.Register(server)
+
+	auth_v1.RegisterAuthServer(server, deliveryGrpc.NewAuthService(nil))
+	log.Printf("server listening at %s\n", conn.Addr())
+
+	if err := server.Serve(conn); err != nil {
+		log.Fatalf("failed to serve %v\n", err)
+	}
+}
---
+++ internal/delivery/grpc/server.go
@@ -0,0 +1,48 @@
+package grpc
+
+import (
+	"balun/auth_service_bln/pkg/auth_v1"
+	"balun/auth_service_bln/vendor/google.golang.org/protobuf/types/known/timestamppb"
+	"context"
+	"fmt"
+	"time"
+
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type AuthServiceGrpc struct {
+	auth_v1.UnimplementedAuthServer
+}
+
+type Config interface {
+}
+
+func NewAuthService(cfg Config) *AuthServiceGrpc {
+	return &AuthServiceGrpc{}
+}
+
+func (a *AuthServiceGrpc) Create(ctx context.Context, req *auth_v1.CreateRequest) (*auth_v1.CreateResponse, error) {
+	fmt.Printf("%+v\n", req)
+	return &auth_v1.CreateResponse{
+		Id: 1,
+	}, nil
+}
+
+func (a *AuthServiceGrpc) Get(ctx context.Context, req *auth_v1.GetRequest) (*auth_v1.GetResponse, error) {
+	return &auth_v1.GetResponse{
+		Id:        1,
+		Name:      "test",
+		Email:     "test@email.com",
+		Role:      auth_v1.Role_user,
+		CreatedAt: timestamppb.New(time.Now()),
+		UpdatedAt: timestamppb.New(time.Now()),
+	}, nil
+}
+
+func (a *AuthServiceGrpc) Update(ctx context.Context, req *auth_v1.UpdateRequest) (*emptypb.Empty, error) {
+	return nil, nil
+}
+
+func (a *AuthServiceGrpc) Delete(ctx context.Context, req *auth_v1.DeleteRequest) (*emptypb.Empty, error) {
+	return nil, nil
+}
