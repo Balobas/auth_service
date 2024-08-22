94036b09937dc7232ec4ec8a5ece919ef6ef845e
vbalobanov <vbalobanov@innoseti.ru>
2024-08-22 23:08:00 +0300

    feat(main.go)[DI_container]: рефактор main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,19 +3,9 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"log"
-	"net"
 
-	"github.com/balobas/auth_service_bln/internal/client/pg"
-	"github.com/balobas/auth_service_bln/internal/config"
-	deliveryGrpc "github.com/balobas/auth_service_bln/internal/delivery/grpc"
-	repositoryPostgres "github.com/balobas/auth_service_bln/internal/repository/postgres"
-	usersService "github.com/balobas/auth_service_bln/internal/service/users"
-	"github.com/balobas/auth_service_bln/pkg/auth_v1"
-
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
+	"github.com/balobas/auth_service_bln/internal/app"
 )
 
 var configPath string
@@ -27,43 +17,14 @@ func init() {
 func main() {
 	flag.Parse()
 
-	if err := config.Load(configPath); err != nil {
-		log.Fatalf("%v", err)
-	}
-
 	ctx := context.Background()
 
-	// Инициализация конфигов
-	pgConfig := config.NewConfigPG()
-	grpcConfig, err := config.NewConfigGRPC()
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-
-	// Инициализация зависимостей
-
-	pgClient, err := pg.NewClient(ctx, pgConfig.DSN())
+	app, err := app.NewApp(ctx, configPath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("successfuly connected to db")
 
-	repo := repositoryPostgres.New(pgClient)
-	usersService := usersService.New(repo)
-
-	// Инициализация сервиса
-	server := grpc.NewServer()
-	reflection.Register(server)
-	auth_v1.RegisterAuthServer(server, deliveryGrpc.NewAuthServerGRPC(nil, usersService))
-
-	// Запуск сервиса
-	conn, err := net.Listen("tcp", grpcConfig.Address())
-	if err != nil {
-		log.Fatalf("failed to listen: %v\n", err)
-	}
-	defer conn.Close()
-
-	if err := server.Serve(conn); err != nil {
-		log.Printf("failed to serve %v\n", err)
+	if err := app.Run(); err != nil {
+		log.Fatal(err)
 	}
 }
