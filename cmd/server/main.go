3aa25ddd155765754dcf578c4f5a706d4d5b44f6
Балобанов Владислав <vbalobanov@zunami.ru>
2024-08-26 18:27:31 +0300

    fix imports

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,21 +4,11 @@ import (
 	"context"
 	"flag"
 	"log"
-  
-	"github.com/balobas/auth_service/internal/app"
-	"github.com/balobas/auth_service/internal/client/pg"
-	"github.com/balobas/auth_service/internal/config"
-	deliveryGrpc "github.com/balobas/auth_service/internal/delivery/grpc"
-	repositoryPostgres "github.com/balobas/auth_service/internal/repository/postgres"
-	usersService "github.com/balobas/auth_service/internal/service/users"
-	"github.com/balobas/auth_service/pkg/auth_v1"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
+	"github.com/balobas/auth_service/internal/app"
 )
 
 var configPath string
-
 
 func init() {
 	flag.StringVar(&configPath, "config-path", "local.env", "path to config file")
