8be9619fd288f8d689b88b897a82ce57a602e8c3
Балобанов Владислав <vbalobanov@zunami.ru>
2025-05-06 23:46:57 +0300

    feat(workers)[permissions]: добавлен воркер для удаления пермишенов у пользователей (необходим чтобы не перегрузить базу одновременным удалением пермишена одного у всех юзеров)

---
+++ internal/worker/permissions_remover/interface.go
@@ -0,0 +1,17 @@
+package workerPermissionsRemover
+
+import (
+	"context"
+	"time"
+)
+
+type Config interface {
+	UsersLimitOnRemovePermission() int64
+	RemoveInterval() time.Duration
+}
+
+type UcPermissions interface {
+	RemovePermissionFromUsers(ctx context.Context, permKey string, limitUsers int64) (anyUsersAffected bool, err error)
+	GetRandomPermissionFromDeletingList(ctx context.Context) (perm string, isListEmpty bool, err error)
+	RemovePermissionFromDeletingList(ctx context.Context, permKey string) error
+}
---
+++ internal/worker/permissions_remover/worker.go
@@ -0,0 +1,75 @@
+package workerPermissionsRemover
+
+import (
+	"context"
+	"log"
+	"time"
+)
+
+type Worker struct {
+	config        Config
+	ucPermissions UcPermissions
+}
+
+func New(
+	config Config,
+	ucPermissions UcPermissions,
+) *Worker {
+	return &Worker{
+		config:        config,
+		ucPermissions: ucPermissions,
+	}
+}
+
+func (w *Worker) Run(ctx context.Context) {
+	log.Printf("start permissions remover worker\n")
+	timer := time.NewTimer(w.config.RemoveInterval())
+	defer timer.Stop() // потестить
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Printf("permissions remover worker stopped. ctx done")
+			return
+		case <-timer.C:
+			select {
+			case <-ctx.Done():
+				log.Printf("permissions remover worker stopped. ctx done")
+				return
+			default:
+			}
+
+			w.removePermissionFromUsers(ctx, w.config.UsersLimitOnRemovePermission())
+			timer.Reset(w.config.RemoveInterval())
+		}
+	}
+}
+
+func (w *Worker) removePermissionFromUsers(ctx context.Context, usersLimit int64) {
+	perm, isPermDeletingListEmpty, err := w.ucPermissions.GetRandomPermissionFromDeletingList(ctx)
+	if err != nil {
+		log.Printf("permissionsRemoverWorker: failed to get random permission from deleting list: %v", err)
+		return
+	}
+	if isPermDeletingListEmpty {
+		return
+	}
+
+	log.Printf("permissionsRemoverWorker: try to remove permission %s from users", perm)
+
+	anyUsersAffected, err := w.ucPermissions.RemovePermissionFromUsers(ctx, perm, usersLimit)
+	if err != nil {
+		log.Printf("permissionsRemoverWorker: failed to remove permission %s from users: %v", perm, err)
+		return
+	}
+
+	if anyUsersAffected {
+		return
+	}
+
+	log.Printf("permissionsRemoverWorker: permission %s was removed from all users having it", perm)
+
+	if err := w.ucPermissions.RemovePermissionFromDeletingList(ctx, perm); err != nil {
+		log.Printf("permissionsRemoverWorker: failed to remove permission from deleting list: %v", err)
+	}
+}
