937d85a859cc225ce3b3ed871bbbb0d5f5f8d6f2
Балобанов Владислав <vbalobanov@zunami.ru>
2025-05-08 16:38:39 +0300

    возврат удаленной миграции с админом

---
+++ migrations/20241219124302_add_admin_user.sql
@@ -0,0 +1,26 @@
+-- +goose Up
+-- +goose StatementBegin
+INSERT INTO users VALUES (
+  'e2a54cc6-814e-4fff-868b-8920c867e705',
+  'admin@admin',
+  'admin',
+  NOW()::TIMESTAMP,
+  NOW()::TIMESTAMP
+);
+
+-- +goose ENVSUB ON
+insert into users_credentials values (
+  'e2a54cc6-814e-4fff-868b-8920c867e705',
+  '${ADMIN_PASSWORD}'
+);
+
+insert into user_permissions (user_uid, permissions) values ('e2a54cc6-814e-4fff-868b-8920c867e705', '{base}');
+-- +goose ENVSUB OFF
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DELETE FROM users WHERE uid = 'e2a54cc6-814e-4fff-868b-8920c867e705';
+DELETE FROM users_credentials WHERE user_uid = 'e2a54cc6-814e-4fff-868b-8920c867e705';
+-- +goose StatementEnd
